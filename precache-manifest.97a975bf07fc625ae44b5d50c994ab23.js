faa0d7bc5ae2ae960c74ef141160d08535d3fb1a
jadejdoucet <jadejdoucet@gmail.com>
2019-12-19 15:09:46 -0600

    Updates

---
+++ precache-manifest.97a975bf07fc625ae44b5d50c994ab23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d9a7e389682f57d40f7d06d0417516",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a1da47da2e1ee0f9e471",
+    "url": "/Portfolio/static/css/main.ce652b67.chunk.css"
+  },
+  {
+    "revision": "f07314f624b359d70726",
+    "url": "/Portfolio/static/js/2.2b7d7833.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/Portfolio/static/js/2.2b7d7833.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1da47da2e1ee0f9e471",
+    "url": "/Portfolio/static/js/main.7981455d.chunk.js"
+  },
+  {
+    "revision": "3d5895fb24bdbec0245e",
+    "url": "/Portfolio/static/js/runtime-main.e9c8fbe3.js"
+  },
+  {
+    "revision": "1dda9e3da59f210c0cefe12b17bc7ad6",
+    "url": "/Portfolio/static/media/headshot-close.1dda9e3d.jpg"
+  }
+]);
