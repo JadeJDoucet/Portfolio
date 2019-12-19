acd5d8ef651de56982b40ececfcf1b23810f27ed
jadejdoucet <jadejdoucet@gmail.com>
2019-12-19 11:13:41 -0600

    added personal profile to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// import components
+import PersonalProfile from './components/PersonalProfile';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <PersonalProfile />
     </div>
   );
 }
--- src/components/PersonalProfile.js
+++ src/components/PersonalProfile.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+// imports
+// import profilePicture from
+
 export default function PersonalProfile() {
 
   return (
     <div id="profile-container">
-      
+      <h1>Jade Doucet</h1>
+      <p>Software Engineer from New Orleans</p>
     </div>
   )
 }
