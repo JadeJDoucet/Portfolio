02288439d1525a37e0952fd1a8d850755b95d540
jadejdoucet <jadejdoucet@gmail.com>
2019-12-19 13:50:56 -0600

    updated profile component

--- src/components/PersonalProfile.js
+++ src/components/PersonalProfile.js
@@ -9,14 +9,16 @@ export default function PersonalProfile() {
   return (
     <div id="profile-card">
       <Card>
+        <Image src={require("../headshots/headshot-close.jpg")} wrapped ui={false} />
         <Card.Content>
-          <Image src="../headshots/headshot-close.jpg" />
         <Card.Header>Jade Doucet</Card.Header>
         <Card.Meta>
             <span className='date'>Software Engineer</span>
         </Card.Meta>
-          <Card.Description>I am a full-stack software engineer from New Orleans</Card.Description>
-          <Button onClick={() => setToggleEmail(!toggleEmail)}>Email</Button>
+          <Card.Description>I am a full-stack software engineer from New Orleans with a passion for technology.
+            I'm experienced in web, mobile, and game development. 
+            I've developed AR mobile apps, a variety of web apps, and video games. </Card.Description>
+          <Button onClick={() => setToggleEmail(!toggleEmail)}>Contact</Button>
           {toggleEmail ? <Card.Description>jadejdoucet@gmail.com</Card.Description> : null}
         </Card.Content>
       </Card>
