c0e91429211de269066235bbf3663d27226e6d94
jadejdoucet <jadejdoucet@gmail.com>
2019-12-19 12:18:06 -0600

    added semantic CDN and basics for profile card

--- src/components/PersonalProfile.js
+++ src/components/PersonalProfile.js
@@ -1,14 +1,24 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Card, Image, Button } from 'semantic-ui-react';
 // imports
 // import profilePicture from
 
 export default function PersonalProfile() {
+  const [toggleEmail, setToggleEmail] = useState(false);
 
   return (
-    <div id="profile-container">
-      <h1>Jade Doucet</h1>
-      <p>Software Engineer from New Orleans</p>
+    <div id="profile-card">
+      <Card>
+        <Card.Content>
+        <Card.Header>Jade Doucet</Card.Header>
+        <Card.Meta>
+            <span className='date'>Software Engineer</span>
+        </Card.Meta>
+          <Button onClick={() => setToggleEmail(!toggleEmail)}>Email</Button>
+          <Card.Description>I am a full-stack software engineer from New Orleans</Card.Description>
+          {toggleEmail ? <Card.Description>jadejdoucet@gmail.com</Card.Description> : null}
+        </Card.Content>
+      </Card>
     </div>
   )
 }
