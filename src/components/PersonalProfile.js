176db340c222c86f604cd490c69c5d32e8940f86
jadejdoucet <jadejdoucet@gmail.com>
2019-12-19 12:35:47 -0600

    adding profile photo

--- src/components/PersonalProfile.js
+++ src/components/PersonalProfile.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Card, Image, Button } from 'semantic-ui-react';
 // imports
-// import profilePicture from
+// import profilePhoto from '../../project-data/headshots/headshot-close.jpg';
 
 export default function PersonalProfile() {
   const [toggleEmail, setToggleEmail] = useState(false);
@@ -10,12 +10,13 @@ export default function PersonalProfile() {
     <div id="profile-card">
       <Card>
         <Card.Content>
+          <Image src="../headshots/headshot-close.jpg" />
         <Card.Header>Jade Doucet</Card.Header>
         <Card.Meta>
             <span className='date'>Software Engineer</span>
         </Card.Meta>
-          <Button onClick={() => setToggleEmail(!toggleEmail)}>Email</Button>
           <Card.Description>I am a full-stack software engineer from New Orleans</Card.Description>
+          <Button onClick={() => setToggleEmail(!toggleEmail)}>Email</Button>
           {toggleEmail ? <Card.Description>jadejdoucet@gmail.com</Card.Description> : null}
         </Card.Content>
       </Card>
