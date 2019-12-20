1c4b2ee5f62518d019a46ad55e088b1ddb19436c
jadejdoucet <jadejdoucet@gmail.com>
2019-12-20 14:58:41 -0600

    added more example data

--- src/project-data/projects.js
+++ src/project-data/projects.js
@@ -1,9 +1,22 @@
 const projects = [
-    {
-        name: 'Scannar',
-        description: 'Something about the app here',
-        images: [],
-    },
+  {
+    name: 'Scannar',
+    description: 'Something about the app here',
+    coverImage: '../pathToImages',
+    images: [],
+  },
+  {
+    name: 'Readr',
+    description: 'Something about', 
+    coverImage: './pathtoImage',
+    images: [],
+  },
+  {
+    name: 'Adopt-A-Pothole',
+    description: 'something here',
+    coverImage: './path here to something',
+    images: [],
+  },
 ];
 
 export default projects;
