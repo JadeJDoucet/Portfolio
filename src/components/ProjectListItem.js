3525578e8001c1848686e042004b5166c1ba193c
jadejdoucet <jadejdoucet@gmail.com>
2019-12-21 22:00:08 -0600

    updated project card styling

--- src/components/ProjectListItem.js
+++ src/components/ProjectListItem.js
@@ -9,7 +9,7 @@ export default function ProjectListItem({ project }) {
   }
 
   return (
-    <Grid.Row style={{ boxShadow: '0px 1px 5px grey' }}>
+    <Grid.Row style={{ boxShadow: '0px 1px 5px grey', borderRadius: 5, marginBottom: 5 }}>
       <Grid.Column width={3}>
         <Image src={coverImage} rounded style={imageStyle}/>
       </Grid.Column>
