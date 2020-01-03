f7b29746b239b5c5a99efdcde1917de70b9ed792
jadejdoucet <jadejdoucet@gmail.com>
2020-01-03 02:58:45 -0600

    removed semantic ui

--- src/App.js
+++ src/App.js
@@ -8,68 +8,9 @@ import ProjectList from './components/ProjectList';
 import NavBar from './components/NavBar';
 
 function App() {
-  const [contact, setContact] = useState(false);
-
-  const styles = {
-    webStyle: {
-      marginTop: '3.8%', // place items under nav bar
-    },
-    mobileStyle: {
-      marginTop: '14%', // place items under nav bar
-    },
-    listCol: {
-      marginLeft: '17.5%',
-      marginTop: '-1.6%',
-    },
-    profileCol: {
-      marginLeft: 5,
-    }
-  }
-  const { webStyle, mobileStyle, listCol, profileCol } = styles;
-
-  const mobileViewMain = () => (
-    <Grid style={{ overflowX: 'hidden' }}>
-      <NavBar mobile setContact={setContact}/>
-      <Grid.Row style={mobileStyle}>
-        <PersonalProfile mobile/>
-      </Grid.Row>
-      <Grid.Row>
-        <ProjectList mobile/>
-      </Grid.Row>
-    </Grid>
-  )
-
-  const desktopViewMain = () => (
-    <div>
-      <NavBar setContact={setContact}/>
-      <div
-        style={webStyle}
-      >
-        <Grid columns={2}>
-          <Grid.Column width={4} style={profileCol}>
-            <PersonalProfile />
-          </Grid.Column>
-          <Grid.Column width={13} style={listCol}>
-            <h1 style={{ textAlign: 'center', fontFamily: 'Work Sans', color: '#fff', textShadow: '1px 0px 1px black' }}>My Projects</h1>
-            <ProjectList />
-          </Grid.Column>
-        </Grid>
-      </div>
-    </div>
-  )
-
-const toRender = () => {
-  if (contact) {
-    // To do: Add dynamic contact page
-    return <div>contact page</div>
-  } else {
-    return window.innerWidth < 1000 ? mobileViewMain() : desktopViewMain();
-  }
-}
-
   return (
     <div style={{ backgroundColor: 'rgb(51, 51, 51)' }}>
-      {toRender()}
+     <PersonalProfile />
     </div>
   );
 }
--- src/components/PersonalProfile.js
+++ src/components/PersonalProfile.js
@@ -1,42 +1,46 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
 
 export default function PersonalProfile({ mobile }) {
-  const fixedProfileStyle = {
-    position: mobile ? null : 'fixed',
-    margin: mobile? 'auto' : null,
-    backgroundColor: 'rgba(255, 255, 255, 0.90)',
-    border: '1px solid #ddd',
-    boxShadow: '0px 1px 2px #fff',
+  // const fixedProfileStyle = {
+  //   position: mobile ? null : 'fixed',
+  //   margin: mobile? 'auto' : null,
+  //   backgroundColor: 'rgba(255, 255, 255, 0.90)',
+  //   border: '1px solid #ddd',
+  //   boxShadow: '0px 1px 2px #fff',
+  // }
+  
+  const styles = {
+    image: {
+      margin: 'auto',
+    },
+    intro: {
+      flex: 1,
+      alignContent: 'center',
+    },
   }
-
+const { image, intro } = styles;
   return (
-    <Card
-      style={fixedProfileStyle}
-    >
-      <Image
+    <div id="intro" style={intro}>
+      <img
       src={require("../project-data/headshots/headshot-close.jpg")}
-      wrapped
-      ui={false}
+      alt="profile"
+      width="20%"
+      style={image}
       />
-      <Card.Content>
-      <Card.Header>Jade Doucet
+      <h1>Jade Doucet
         <a href="https://github.com/JadeJDoucet" style={{ marginLeft: '25%', color: 'black', fontSize: 25 }}>
           <i className="github icon"></i>
         </a>
           <a href="https://linkedin.com/in/jadejdoucet" style={{ color: '#0077B5', fontSize: 25 }}>
           <i className="linkedin icon"></i>
         </a>
-      </Card.Header>
-      <Card.Meta>
+      </h1>
           <span className='date'>Software Engineer</span>
-      </Card.Meta>
-        <Card.Description>I am a full-stack software engineer from New Orleans with a passion for technology.
+        <p>I am a full-stack software engineer from New Orleans with a passion for technology.
           I've built web and mobile apps with JavaScript and Node, as well as video games within Unity. 
           I'm proficient in full stack development, whether it's designing a front-end or back-end, dealing with APIs, deploying, or constructing databases with SQL or NoSQL.
           For any questions, feel free to contact me via <a href="mailto:jadejdoucet@gmail.com">email</a>.
-        </Card.Description>
-      </Card.Content>
-    </Card>
+        </p>
+    </div>
   )
 }
