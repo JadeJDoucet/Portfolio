7b3e2f8946471accadd3555f7165819c4661e398
jadejdoucet <jadejdoucet@gmail.com>
2020-01-03 16:41:09 -0600

    added dynamic margin for tool lists

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Grid, List, ListItem, ListItemText, Typography } from '@material-ui/core';
 
 export default function Tools() {
+  const styles = {
+    toolWrapper: {
+      marginLeft: window.innerWidth > 1000 ? '40%' : '15%',
+    },
+  };
+
+  const { toolWrapper } = styles;
+
   return (
     <section id="tools" style={{ textAlign: 'center' }}>
       <Grid container spacing={2}>
         <Grid item xs={12} style={{ paddingTop: '3%' }}>
-          <Typography variant="h5">My Tools </Typography>
+          <Typography variant="h4">My Tools </Typography>
         </Grid>
         <Grid item xs={6}>
           <Typography variant="h6" style={{ textDecoration: 'underline' }}>Front-End</Typography>
-          <List>
+          <List style={toolWrapper}>
             <ListItem>
               <ListItemText variant="body1">- HTML / CSS</ListItemText>
             </ListItem>
@@ -30,7 +38,7 @@ export default function Tools() {
         </Grid>
         <Grid item xs={6}>
           <Typography variant="h6" style={{ textDecoration: 'underline' }}>Back-End</Typography>
-          <List>
+          <List style={toolWrapper}>
             <ListItem>
               <ListItemText variant="body1">- Node.js</ListItemText>
             </ListItem>
