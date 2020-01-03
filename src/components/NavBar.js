d5601a241040335afefa229bf9846cb49df65d25
jadejdoucet <jadejdoucet@gmail.com>
2020-01-03 12:46:25 -0600

    updated navigation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,6 @@
-import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
-
-function handleClick(loc) {
-  /* switch this for deployment */
-  window.location = 'jadejdoucet.dev' + loc;
-  // window.location = 'localhost:3000' + loc;
-}
 
 export default function NavBar() {
   const styles = {
@@ -15,7 +8,7 @@ export default function NavBar() {
       padding: '0.5%',
       paddingLeft: '1%',
       alignText: 'right',
-      position: 'sticky'
+      // position: 'sticky'
     },
   };
 
@@ -26,40 +19,12 @@ export default function NavBar() {
       <Link color="textPrimary" href="#about">
         About
       </Link>
-      <Link color="inherit" onClick={() => handleClick('#about')}>
+      <Link color="inherit" href="#projects">
         Projects
       </Link>
-      <Link color="inherit" onClick={() => handleClick('#contact')}>
+      <Link color="inherit" href="#contact">
         Contact
       </Link>
     </Breadcrumbs>
   )
-
-  // const styles = {
-  //   navStartBox: {
-  //     backgroundColor: '#fff',
-  //     padding: 'auto',
-  //     display: 'inline'
-  //   },
-  //   navWrapper: {
-  //     height: '2em',
-  //     width: '100%',
-  //     display: 'inline-block',
-  //     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-  //     // position: 'absolute',
-  //   },
-  //   logo: {
-  //     marginTop: '0.5%',
-  //     marginLeft: '1%'
-  //   },
-  // };
-
-  // const { navWrapper, logo, navStartBox } = styles;
-  // return (
-  //   <div style={navWrapper}>
-  //     <div style={navStartBox}>
-  //     </div>
-  //       <h5 style={logo}>jadejdoucet.dev</h5>
-  //     </div>
-  // );
 }
