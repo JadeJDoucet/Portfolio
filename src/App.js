d4a037491d66c0784d29e179f1bbd72257ffedc5
jadejdoucet <jadejdoucet@gmail.com>
2020-01-02 21:44:25 -0600

    added skeleton of web page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ import PersonalProfile from './components/PersonalProfile';
 // import NavBar from './components/NavBar';
 
 function App() {
+  // const styles = {
+  //   container: {
+  //     backgroundColor: 'rgb(51, 51, 51)',
+  //     display: 'flex',
+  //     flexDirection: 'column',
+  //     margin: 'auto'
+  //   },
+  // };
+
+  // const { container } = styles;
   return (
-    <div style={{ backgroundColor: 'rgb(51, 51, 51)', display: 'flex', margin: 'auto' }}>
+    <div>
      <PersonalProfile />
     </div>
   );
--- src/components/PersonalProfile.js
+++ src/components/PersonalProfile.js
@@ -1,31 +1,40 @@
 import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 export default function PersonalProfile({ mobile }) {
   const styles = {
+    introWrapper: {
+      backgroundColor: 'rgb(51, 51, 51)',
+      textAlign: 'center',
+      display: 'inline-block',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    },
     image: {
-      margin: 'auto',
-    },
-    intro: {
-      alignSelf: 'center',
-    },
+      display: 'block',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: 3,
+      width: '23%',
+      borderRadius: '50%',
+    }
   }
-const { image, intro } = styles;
+const { image, introWrapper } = styles;
   return (
-    <div id="intro" style={intro}>
+    <div id="intro" style={introWrapper}>
       <img
       src={require("../project-data/headshots/headshot-close.jpg")}
       alt="profile"
-      width="20%"
       style={image}
       />
       <h1>Jade Doucet
-        <a href="https://github.com/JadeJDoucet" style={{ marginLeft: '25%', color: 'black', fontSize: 25 }}>
-          <i className="github icon"></i>
-        </a>
-          <a href="https://linkedin.com/in/jadejdoucet" style={{ color: '#0077B5', fontSize: 25 }}>
-          <i className="linkedin icon"></i>
-        </a>
       </h1>
+        <a href="https://github.com/JadeJDoucet" style={{ marginLeft: '25%', color: 'black', padding: '5px' }}>
+          <GitHubIcon />
+        </a>
+          <a href="https://linkedin.com/in/jadejdoucet" style={{ color: '#0077B5' }}>
+          <LinkedInIcon />
+        </a>
           <span className='date'>Software Engineer</span>
         <p>I am a full-stack software engineer from New Orleans with a passion for technology.
           I've built web and mobile apps with JavaScript and Node, as well as video games within Unity. 
