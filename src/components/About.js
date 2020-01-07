fd7e0304612d9635872d6c8d601c8bdac877ad30
jadejdoucet <jadejdoucet@gmail.com>
2020-01-07 10:24:21 -0600

    updated spacing

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core';
 export default function About({ mobile }) {
   const styles = {
     introWrapper: {
-      textAlign: 'center',
+      textAlign: 'justify',
       display: 'inline-block',
       boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
       padding: '5%',
@@ -15,7 +15,7 @@ export default function About({ mobile }) {
   const { introWrapper } = styles;
   return (
     <section id="about" style={introWrapper}>
-      <Typography variant="h4" style={{ marginTop: '-3%', paddingBottom: '3%' }}>About Me</Typography>
+      <Typography variant="h4" style={{ marginTop: '-3%', paddingBottom: '3%', textAlign: 'center' }}>About Me</Typography>
       <Typography variant="body1">I am a full-stack software engineer from New Orleans with a passion for technology.
         I've built web and mobile apps with JavaScript and Node, as well as video games within Unity.
         I'm proficient in full stack development, whether it's designing a front-end or back-end, dealing with APIs, deploying, or constructing databases with SQL or NoSQL.
